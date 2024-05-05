b17529b05b7a2d817c94dc51599d69921eaa739b
Kirill <pyasecky.code@zohomail.eu>
2024-05-05 16:31:26 +0300

    Add AssetManager class base

---
+++ core/asset_manager/AssetManager.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+
+#include "core/includes/Includes.h"
+
+namespace qtproject
+{
+    namespace data
+    {
+        class AssetManager {
+            private:
+            inline static AssetManager* manager_{nullptr};
+            std::ostream* os_{nullptr};
+            std::istream* is_{nullptr};
+
+            AssetManager(std::ostream* os, std::istream* is) : os_(os), is_(is) {
+            }
+
+            AssetManager(std::istream* is) : is_(is) {
+            }
+
+            public:
+            static AssetManager& Init(std::ostream* os, std::istream* is = nullptr) {
+                if (manager_ != nullptr)
+                    delete manager_;
+                manager_ = new AssetManager(os, is);
+                return *manager_;
+            }
+
+            static AssetManager& Init(std::istream* is) {
+                if (manager_ != nullptr)
+                    delete manager_;
+                manager_ = new AssetManager(is);
+                return *manager_;
+            }
+
+
+            static bool InitedToWrite() {
+                return manager_ != nullptr && manager_->os_ != nullptr;
+            }
+
+            static bool InitedToRead() {
+                return manager_ != nullptr && manager_->is_ != nullptr;
+            }
+
+            static void Close() {
+                delete manager_;
+                manager_ = nullptr;
+            }
+
+        };
+    }
+}
---
+++ core/asset_manager/test.cpp
@@ -0,0 +1,7 @@
+#include "core/asset_manager/AssetManager.h"
+
+
+int main() {
+    std::cout << "Hello, World\n";
+    return 0;
+}
