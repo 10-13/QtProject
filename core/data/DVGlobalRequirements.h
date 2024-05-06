9cbab384446eca85b52c9d1cbd25de2b9cf2972b
10-13 <vit.tit.mid@gmail.com>
2024-05-07 00:09:11 +0300

    Add require macro base

--- core/data/DVGlobalRequirements.h
+++ core/data/DVGlobalRequirements.h
@@ -6,20 +6,28 @@ namespace qtproject {
         namespace require
         {
             struct Requirement {
+            private:
+                using BuildMethod = void(*)(std::shared_ptr<qtproject::data::DValue> req);
+            public:
                 std::string Name;
                 StructureChecker Checker;
+                Requirement(std::string Name, BuildMethod builder) {
+                    builder(Checker.Pattern);
+                }
             };
 
             class GlobalTemplates {
             private:
-                std::map<std::string, Requirement*> Templates_;
+                std::map<std::string, StructureChecker> Templates_;
 
             public:
                 StructureChecker& GetByRequirementName(std::string Name) {
-                    return Templates_[Name]->Checker;
+                    return Templates_[Name];
                 }
                 void AddRequirement(Requirement* req) {
-                    Templates_[req->Name] = req;
+                    if(Templates_.find(req->Name) == Templates_.end())
+                        Templates_[req->Name] = StructureChecker{};
+                    req->Checker.RestorePathes(Templates_[req->Name].Pattern);
                 }
             };
         }
---
+++ core/data/DVReqMacro.h
@@ -0,0 +1,23 @@
+#include "DVGlobalRequirements.h"
+
+#define __MERGE(a,b) a##b
+#define __MERGE2(a,b) __MERGE(a, b)
+
+#define __DV_BUILD_NAME() __MERGE2(__dv_req,__COUNTER__)
+
+#define __SRC_DV_REQ(nameArg, PID) \
+    void __bm##PID(std::shared_ptr<qtproject::data::DValue>); \
+    qtproject::data::require::Requirement* req = new qtproject::data::require::Requirement(nameArg, __bm##PID); \
+    void __bm##PID(std::shared_ptr<qtproject::data::DValue> pattern) 
+
+#define __SRC_DV_REQ2(nameArg, PID) __SRC_DV_REQ(nameArg, PID)
+
+#define DV_REQUIREMENT(name) __SRC_DV_REQ2(name, __DV_BUILD_NAME())
+
+
+
+DV_REQUIREMENT("block") {
+    pattern->Add("Type");
+    pattern->Add("Firm");
+    pattern->Add
+}
